0b8a6001d12284a136fe424cf681fbd70c4e8f58
assekochrist <christkiller7@gmail.com>
2026-06-24 16:12:56 +0000

    interfaces2

--- resources/views/admin/dashboard_content.blade.php
+++ resources/views/admin/dashboard_content.blade.php
@@ -8,7 +8,7 @@
 <section class="actions">
     <h2>⚡ Actions rapides</h2>
     <div class="actions-container">
-        <button class="action-btn">➕ Programmer un voyage</button>
+        <button class="action-btn">➕ Programmer tttt un voyage</button>
         <button class="action-btn">👤 Ajouter un personnel</button>
         <button class="action-btn">🏢 Ajouter une gare</button>
         <button class="action-btn">💳 Gérer les paiements</button>
